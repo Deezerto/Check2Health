4aa831498245f9b6d91f6501c16a28ddc99c1d2a
german <germanolivervelascoiv2@gmail.com>
2025-12-07 17:37:11 +0800

    Removed unused imports

--- src/main/java/com/appdev/lastico/check2health/Service/ReservationService.java
+++ src/main/java/com/appdev/lastico/check2health/Service/ReservationService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import com.appdev.lastico.check2health.DTO.ConsultationDetailsDTO;
 import java.util.List;
 import java.util.Map;
